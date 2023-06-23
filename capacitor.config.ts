04d4cc734c7b8ba5ecc8f2704eee345c19b57f29
AlexRTC1997 <aturpoco@unsa.edu.pe>
2023-06-23 12:12:10 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GES_Big_Times_Projects',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
